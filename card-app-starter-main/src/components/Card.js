f4138bc7c6f795789aa24b95f1a5aa91722c1ae7
JenelleNg <24013515@myrp.edu.sg>
2026-01-27 09:42:45 +0800

    potatoes

--- card-app-starter-main/src/components/Card.js
+++ card-app-starter-main/src/components/Card.js
@@ -22,34 +22,3 @@ export default function Card({ card, onDelete, busy }) {
     </div>
   );
 }
-
-// import { Link } from "react-router-dom";
-
-// export default function Card({ card, onDelete, disabled }) {
-//   return (
-//     <div className="card">
-//       <div className="card__imgWrap">
-//         <img className="card__img" src={card.card_pic} alt={card.card_name} />
-//       </div>
-
-//       <div className="card__body">
-//         <h3 className="card__title">{card.card_name}</h3>
-//         <p className="muted small">ID: {card.id}</p>
-
-//         <div className="actions actions--tight">
-//           <Link className="btn btn--ghost" to={`/cards/${card.id}/edit`}>
-//             Edit
-//           </Link>
-//           <button
-//             className="btn btn--danger"
-//             onClick={() => onDelete(card)}
-//             disabled={disabled}
-//             type="button"
-//           >
-//             Delete
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
